d99a0ecf6c231e538da79fec9649b7f5ff53f7f4
Ankang Hu <23111220065@m.fudan.edu.cn>
2025-05-23 16:53:23 +0800

    test

---
+++ mytest.py
@@ -0,0 +1,43 @@
+#%%
+import os
+import spikeinterface.full as si
+from spikeinterface.extractors import toy_example
+from spikeinterface.sorters import run_sorter
+wd = r"F:\GitHub\anipose_m\Mojackhak\spiketutorials\test"
+os.chdir(wd)
+si.available_sorters()
+# si.installed_sorters()
+
+#%%
+recording, _ = toy_example(
+    duration=30,
+    seed=0,
+    num_channels=64,
+    num_segments=1
+)
+recording = recording.save(folder=r"F:\GitHub\anipose_m\Mojackhak\spiketutorials\test\test-docker-folder", overwrite=True)
+
+#%%
+sorting_TDC = run_sorter(sorter_name="tridesclous", recording=recording, detect_threshold=4)
+print(sorting_TDC)
+
+# %%
+sorting_SC2 = run_sorter(sorter_name="spykingcircus2", recording=recording)
+
+# %%
+sorting_KS2 = run_sorter(sorter_name="kilosort2", recording=recording, 
+                         installation_mode='pypi',
+                         docker_image=True, verbose=True)
+# %%
+sorting_SC2 = run_sorter(sorter_name="spykingcircus2", recording=recording)
+
+
+#%%
+run_sorter(sorter_name='waveclus', recording=recording, 
+           docker_image='mojack/waveclus',
+        #    installation_mode='pypi',
+           verbose=True)
+# %%
+sorting_KS2_5 = run_sorter(sorter_name="kilosort2_5", recording=recording,
+                           docker_image='spikeinterface/kilosort2_5-compiled-base')
+# %%
